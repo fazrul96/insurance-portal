5f2d5e929089cce977188c9c91c238e84e4cf727
Fazrul_Romli <muhammad.f.romli@accenture.com>
2025-04-29 09:50:07 +0800

    remove unused component

--- src/app/features/policy-servicing/policy-servicing.component.ts
+++ src/app/features/policy-servicing/policy-servicing.component.ts
@@ -1,18 +1,9 @@
-import { Component } from '@angular/core';
-import {TableComponent} from '../../shared/components/table/table.component';
-import {NxBreadcrumbComponent, NxBreadcrumbItemComponent} from '@aposin/ng-aquila/breadcrumb';
-import {RouterLink} from '@angular/router';
-import {NxLayoutComponent} from '@aposin/ng-aquila/grid';
+import {Component} from '@angular/core';
 import {PolicyServicingNewComponent} from '../policy-servicing-new/policy-servicing-new.component';
 
 @Component({
   selector: 'app-policy-servicing',
   imports: [
-    TableComponent,
-    NxBreadcrumbComponent,
-    NxBreadcrumbItemComponent,
-    RouterLink,
-    NxLayoutComponent,
     PolicyServicingNewComponent
   ],
   templateUrl: './policy-servicing.component.html',
